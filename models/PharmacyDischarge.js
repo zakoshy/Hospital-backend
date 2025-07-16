046dc8ca56e05bb4de7fb9de4ce35818aeed917c
Zack Edwin <zakayooshome254@gmail-com>
2025-07-16 17:46:24 +0300

    Fix file name casing for PharmacyDischarge model

--- models/PharmacyDischarge.js
+++ models/PharmacyDischarge.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const PharmacyDischargeSchema = new mongoose.Schema({
   patientId: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" },
   unavailableMeds: [String],
-  dischargedAt: { type: Date, default: Date.now }
+  dischargedAt: { type: Date, default: Date.now } 
 });
 
 module.exports = mongoose.model("PharmacyDischarge", PharmacyDischargeSchema);
